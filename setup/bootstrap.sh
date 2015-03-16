3fb871474415572622b4f68dfdf0b2e21d5299f6
Ralph Yozzo <ralph@brooklynmarathon.com>
2015-03-15 23:23:56 -0400

    Update bootstrap.sh

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -98,7 +98,8 @@ if [ ! -f "/opt/redash/.env" ]; then
 fi
 
 # Install latest version
-REDASH_VERSION=${REDASH_VERSION-0.4.0.b589}
+# REDASH_VERSION=${REDASH_VERSION-0.4.0.b589}
+REDASH_VERSION=${REDASH_VERSION-0.6.0.b722}
 LATEST_URL="https://github.com/EverythingMe/redash/releases/download/v${REDASH_VERSION/.b/%2Bb}/redash.$REDASH_VERSION.tar.gz"
 VERSION_DIR="/opt/redash/redash.$REDASH_VERSION"
 REDASH_TARBALL=/tmp/redash.tar.gz
